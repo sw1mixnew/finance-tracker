9e733d67d2fe19feb82142ddcbe7d071ab8a50a7
user <me@example.com>
2026-08-26 16:32:04 +0300

    Add PWA: offline support + home screen icon

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'finance-v1';
+const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((r) => {
+        const copy = r.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return r;
+      })
+      .catch(() => caches.match(e.request).then((m) => m || caches.match('./index.html')))
+  );
+});
